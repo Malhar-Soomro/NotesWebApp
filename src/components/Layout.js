658d902982f03837f6547cec8a15340f42e41279
Malhar Soomro <77386938+Malhar-Soomro@users.noreply.github.com>
2021-10-17 23:37:27 +0500

    topBar added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,40 @@
 import { makeStyles } from '@material-ui/styles'
 import React from 'react'
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Typography } from '@material-ui/core';
+import { format } from 'date-fns'
 
 const useStyle = makeStyles({
     layout: {
         backgroundColor: "#f4f4f4",
         width: "100%"
+    },
+    appbar: {
+        backgroundColor: "#ffff",
+        // marginBottom: 8
     }
 });
+
 const Layout = ({ children }) => {
     const classes = useStyle();
     return (
-        <div>
-
-            <div className={classes.layout}>
+        <div className={classes.layout}>
+            <AppBar
+                className={classes.appbar}
+                position="static"
+                color="transparent"
+                elevation={0}
+            >
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                    >
+                        Today is {format(new Date(), 'do MMMM Y')}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <div>
                 {children}
             </div>
         </div>
